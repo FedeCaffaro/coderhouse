80af31f0311b0bf9866ea3f1a0c71ae132b47638
FedeCaffaro <fcaffaro@itba.edu.ar>
2023-02-21 20:15:30 -0300

    asd

--- desafio-5/src/config/options.js
+++ desafio-5/src/config/options.js
@@ -1,10 +1,10 @@
-export const options = {
-    fileSystem: {
-      cartsFileName: "Carts.json",
-      productsFileName: "Products.json",
-    },
-    mongoDb: {
-      url: "mongodb+srv://fedecaffaro:coder@cluster0.zbptslw.mongodb.net/ecommerce?retryWrites=true&w=majority",
-    },
-  };
-  
+const options = {
+  fileSystem: {
+      productsFileName: "products.json"
+  },
+  mongoDB:{
+      url: "mongodb+srv://fedecaffaro:coder@cluster0.zbptslw.mongodb.net/ecommerce?retryWrites=true&w=majority"
+  }
+}
+
+module.exports = options
